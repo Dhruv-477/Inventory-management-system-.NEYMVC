07e136d3bd8a148839c3308b099de899820f1647
Dhruv Verma <165140688+Dhruv-477@users.noreply.github.com>
2025-12-25 13:29:33 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myFirstapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace myFirstapp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(150)]
+        public string Name { get; set; }
+
+        [Required, StringLength(150)]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Required] 
+        [Column(TypeName ="decimal(18,2)")]
+        [Range(0.0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
